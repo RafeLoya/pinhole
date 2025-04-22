7b6bf9b569e11a092cb0b4dd7ea5c0144a5b6e89
rafeloya <rafe_loya1@baylor.edu>
2025-04-22 10:31:29 -0500

    'ascii-webcam-demo' Integration

---
+++ client/src/ascii_converter.rs
@@ -0,0 +1,195 @@
+use std::error::Error;
+use crate::ascii_frame::AsciiFrame;
+use crate::edge_detector::EdgeDetector;
+use crate::image_frame::ImageFrame;
+
+/// The coefficients below are derived from Rec. ITU-R BT.601-7.
+/// In the specification, these luminance coefficients represent
+/// how much they influence / contribute to the human eye's 
+/// perception of brightness.
+
+pub const R_LUMINANCE: f32 = 0.2989;
+pub const G_LUMINANCE: f32 = 0.5870;
+pub const B_LUMINANCE: f32 = 0.1140;
+
+/// Intermediary translator to transform an `ImageFrame` into an `AsciiFrame`
+pub struct AsciiConverter {
+    /// Identifies edges in given `ImageFrame`s
+    edge_detector: EdgeDetector,
+    /// Intensity for `AsciiFrame` "pixels"
+    ascii_intensity: Vec<char>,
+    /// Characters for horizontal edges in `AsciiFrame` representation
+    ///
+    /// NOTE: 'horizontal' is in reference to the gradient direction, not
+    /// their angle in the original / ASCII image, so these will actually 
+    /// be vertical characters in the final output (i.e. "|│┃")
+    ascii_horizontal: Vec<char>,
+    /// Characters for vertical edges in `AsciiFrame` representation
+    ///
+    /// NOTE: 'vertical' here means is in reference to the gradient direction,
+    /// not their angle in the original / ASCII image, so these will actually
+    /// be horizontal characters in the final output (i.e. "-━═")
+    ascii_vertical: Vec<char>,
+    /// Characters for forward edges in `AsciiFrame` representation
+    ascii_forward: Vec<char>,
+    /// Characters for back edges in `AsciiFrame` representation
+    ascii_back: Vec<char>,
+    /// Minimum gradient magnitude for edge detection
+    edge_threshold: f32,
+    /// Adjustment factor for contrast.
+    /// Values < 1.0 reduce contrast, values > 1.0 increase contrast
+    contrast: f32,
+    /// Adjustment factor for brightness.
+    /// values > 0 increase brightness, values < 0 brightness
+    brightness: f32,
+}
+
+impl AsciiConverter {
+    pub const DEFAULT_ASCII_INTENSITY: &'static str = " .:coPO?@■";
+    pub const DEFAULT_ASCII_VERTICAL: &'static str = "-━═";
+    pub const DEFAULT_ASCII_HORIZONTAL: &'static str = "|│┃";
+    pub const DEFAULT_ASCII_FORWARD: &'static str = "/╱⟋";
+    pub const DEFAULT_ASCII_BACK: &'static str = "\\╲⟍";
+    pub const DEFAULT_CONTRAST: f32 = 1.5;
+    pub const DEFAULT_BRIGHTNESS: f32 = 0.0;
+
+    pub fn new(
+        ascii_intensity: Vec<char>,
+        ascii_horizontal: Vec<char>,
+        ascii_vertical: Vec<char>,
+        ascii_forward: Vec<char>,
+        ascii_back: Vec<char>,
+        w: usize,
+        h: usize,
+        edge_threshold: f32,
+        contrast: f32,
+        brightness: f32
+    ) -> Result<Self,  Box<dyn Error>> {
+
+        let edge_detector = EdgeDetector::new(w, h, edge_threshold);
+
+        edge_detector.start(w, h)?;
+
+        Ok(Self {
+            edge_detector,
+            ascii_intensity,
+            ascii_horizontal,
+            ascii_vertical,
+            ascii_forward,
+            ascii_back,
+            edge_threshold,
+            contrast,
+            brightness
+        })
+    }
+
+    pub fn default() -> Result<Self,  Box<dyn Error>> {
+        Self::new(
+            Self::DEFAULT_ASCII_INTENSITY.chars().collect(),
+            Self::DEFAULT_ASCII_HORIZONTAL.chars().collect(),
+            Self::DEFAULT_ASCII_VERTICAL.chars().collect(),
+            Self::DEFAULT_ASCII_FORWARD.chars().collect(),
+            Self::DEFAULT_ASCII_BACK.chars().collect(),
+            640,
+            480,
+            EdgeDetector::DEFAULT_EDGE_THRESHOLD,
+            Self::DEFAULT_CONTRAST,
+            Self::DEFAULT_BRIGHTNESS,
+        )
+    }
+
+    /// Convert an `ImageFrame` to an ASCII art representation with edges
+    /// - Strong edges (based on `edge_threshold`) are represented with 
+    ///   separate characters to reflect the angle of an edge
+    /// - All other regions are represented with intensity-based (grayscale)
+    ///   ASCII characters
+    ///
+    /// The function also handles scaling from the original `ImageFrame`'s
+    /// dimensions to the target `AsciiFrame`'s dimensions
+    pub fn convert(&self, i_frame: &ImageFrame, a_frame: &mut AsciiFrame) -> Result<(), Box<dyn Error>> {
+        // submit the original image to the edge detector
+        self.edge_detector.submit_frame(i_frame)?;
+
+        // scaling factors to map the ASCII frame's dimension 
+        // to the original image's dimension
+        let scale_x = i_frame.w as f32 / a_frame.w as f32;
+        let scale_y = i_frame.h as f32 / a_frame.h as f32;
+
+        // retrieve processed edge info
+        let edge_info = self.edge_detector.get_edge_info()?;
+
+        for y in  0..a_frame.h {
+            for x in  0..a_frame.w {
+                let i_x = (x as f32 * scale_x) as usize;
+                let i_y = (y as f32 * scale_y) as usize;
+                let e_i = i_y.min(edge_info.h - 1)
+                    * edge_info.w
+                    + i_x.min(edge_info.w - 1);
+
+                // if an edge's magnitude is greater than the threshold,
+                // assign edge character instead of regular character
+                if e_i < edge_info.magnitude.len()
+                    && edge_info.magnitude[e_i] > self.edge_threshold {
+
+                    let c = self.angle_to_edge(edge_info.angle[e_i], edge_info.magnitude[e_i]);
+                    a_frame.set_char(x, y, c);
+                } else {
+                    // No significant edge, retrieve RGB values from
+                    // scaled pixel destination in image frame and
+                    // map by intensity
+                    if let Some(rgb) = i_frame.get_pixel(i_x, i_y) {
+                        // modify RGB w/ given brightness & contrast values
+                        let rgb_adj = self.adjust_pixel(rgb);
+                        let intensity = ImageFrame::calculate_intensity_u8(rgb_adj);
+
+                        let char_i = (intensity as f32 / 255.0 * self.ascii_intensity.len() as f32) as usize;
+                        // bounds check (e.g. floating point rounding error)
+                        let char_i = char_i.min(self.ascii_intensity.len() - 1);
+
+                        a_frame.set_char(x, y, self.ascii_intensity[char_i]);
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Alter the color channels of an RGB pixel according to the specified
+    /// `contrast` and `brightness` values.
+    fn adjust_pixel(&self, (r, g, b): (u8, u8, u8)) -> (u8, u8, u8) {
+        // closure to independently modify RGB channels
+        let apply = |value: u8| -> u8 {
+            // normalize color value (0-255) between 0.0 and 1.0
+            let mut v = value as f32 / 255.0;
+            v = (v - 0.5) * self.contrast + 0.5;
+            v += self.brightness;
+            // floor of 0.0 and ceiling of 1.0 (prevent overflow)
+            v = v.max(0.0).min(1.0);
+            (v * 255.0) as u8
+        };
+
+        (apply(r), apply(g), apply(b))
+    }
+
+    /// Normalizes an angle to 0-180 degrees, then maps the angle to an
+    /// angle character based on magnitude and angle degree
+    fn angle_to_edge(&self, angle: f32, magnitude: f32) -> char {
+        // normalizing to 0-180
+        let angle_d = ((angle.to_degrees() % 180.0) + 180.0) %  180.0;
+
+        let char_i = ((magnitude / 255.0)
+            * (self.ascii_horizontal.len() as f32)).min(
+            (self.ascii_horizontal.len() - 1) as f32) as usize;
+
+        if (angle_d >= 0.0 && angle_d < 22.5) || (angle_d >= 157.5 && angle_d < 180.0) {
+            self.ascii_horizontal[char_i.min(self.ascii_horizontal.len() - 1)]
+        } else if (angle_d >= 22.5) && (angle_d < 67.5) {
+            self.ascii_forward[char_i.min(self.ascii_forward.len() - 1)]
+        } else if (angle_d >= 67.5) && (angle_d < 112.5) {
+            self.ascii_vertical[char_i.min(self.ascii_vertical.len() - 1)]
+        } else {
+            self.ascii_back[char_i.min(self.ascii_back.len() - 1)]
+        }
+    }
+}
---
+++ client/src/edge_detector.rs
@@ -0,0 +1,287 @@
+use crate::image_frame::ImageFrame;
+use std::error::Error;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+// TODO: Look into Robert's Cross operator as potential alternative (if slow performance)
+// TODO: Remove `.unwrap()`s in the future for error recovery
+// TODO: Allow user to influence `threshold` data member
+
+pub struct EdgeInfo {
+    /// the strength / intensity of an edge, if it exists
+    pub magnitude: Vec<f32>,
+    /// the angle of an edge, if it exists
+    pub angle: Vec<f32>,
+    /// the width of the camera it will receive image frames from
+    pub w: usize,
+    /// the height of the camera it will receive image frames from
+    pub h: usize,
+}
+
+pub struct EdgeDetector {
+    /// The edge magnitudes and angles of the latest processed `ImageFrame`.
+    edge_info: Arc<Mutex<EdgeInfo>>,
+    /// The raw image data of the latest processed `ImageFrame`
+    frame_buffer: Arc<Mutex<Vec<u8>>>,
+    /// Flag, indicates to `EdgeDetector` that there is a new `ImageFrame`
+    /// loaded in `frame_buffer`
+    new_frame_available: Arc<Mutex<bool>>,
+    /// Minimum gradient magnitude threshold.
+    /// Operates from 0.0 to 255.0
+    threshold: f32,
+    /// Control flag, will terminate the edge detection thread when `false`
+    running: Arc<Mutex<bool>>,
+}
+
+impl EdgeDetector {
+    /// default `threshold` value if none is provided
+    pub const DEFAULT_EDGE_THRESHOLD: f32 = 20.0;
+
+    pub fn new(w: usize, h: usize, threshold: f32) -> Self {
+        let edge_info = Arc::new(Mutex::new(EdgeInfo {
+            magnitude: vec![0.0; w * h],
+            angle: vec![0.0; w * h],
+            w,
+            h,
+        }));
+
+        let frame_buffer =  Arc::new(Mutex::new(
+            Vec::<u8>::with_capacity(w * h * 3))
+        );
+        let new_frame_available = Arc::new(Mutex::new(false));
+        let running = Arc::new(Mutex::new(true));
+
+        Self {
+            edge_info,
+            frame_buffer,
+            new_frame_available,
+            threshold,
+            running
+        }
+    }
+
+    /// Launches the edge detection processing thread.
+    ///
+    /// This processing thread continuously checks for new frames produced by
+    /// a camera, then processes them with various algorithms to obtain
+    /// edge information. Communication between threads is enforced by
+    /// shared state protected with mutexes.
+    ///
+    /// # Returns
+    ///
+    /// `JoinHandle` for the edge detection processing thread, to manage
+    /// or complete its lifetime.
+    pub fn start(&self, cam_w: usize, cam_h: usize) -> Result<thread::JoinHandle<()>, Box<dyn Error>> {
+        let edge_info = Arc::clone(&self.edge_info);
+        let frame_buffer = Arc::clone(&self.frame_buffer);
+        let new_frame_flag = Arc::clone(&self.new_frame_available);
+        let running = Arc::clone(&self.running);
+        let threshold = self.threshold;
+
+        let handle = thread::spawn(move || {
+            while *running.lock().unwrap() {
+                let process_frame = {
+                    let mut flag = new_frame_flag.lock().unwrap();
+                    let should_proc = *flag;
+                    *flag = false;
+                    should_proc
+                };
+
+                if process_frame {
+                    let frame_data = frame_buffer.lock().unwrap().clone();
+
+                    let temp_frame = ImageFrame {
+                        w: cam_w,
+                        h: cam_h,
+                        bytes_per_pixel: 3,
+                        buffer: frame_data,
+                    };
+
+                    if let Ok((magnitude, angle)) = Self::process_frame(&temp_frame, threshold) {
+                        let mut info = edge_info.lock().unwrap();
+                        info.magnitude = magnitude;
+                        info.angle = angle;
+                    }
+                } else {
+                    thread::sleep(Duration::from_millis(5));
+                }
+            }
+        });
+
+        Ok(handle)
+    }
+
+    /// Utilized by the main program thread to send video frames to
+    /// the edge detection thread to be processed
+    pub fn submit_frame(&self, frame: &ImageFrame) -> Result<(), Box<dyn Error>> {
+        let mut buffer = self.frame_buffer.lock().unwrap();
+
+        buffer.clear();
+        buffer.extend_from_slice(frame.buffer());
+
+        let mut flag = self.new_frame_available.lock().unwrap();
+        *flag = true;
+
+        Ok(())
+    }
+
+    /// Using the Sobel operator, processes an image frame fo edge detection
+    /// after retrieving the grayscale intensity map
+    fn process_frame(frame: &ImageFrame, threshold: f32) -> Result<(Vec<f32>, Vec<f32>), Box<dyn Error>> {
+        let intensity = Self::create_intensity_map(frame);
+        let (gx, gy) = Self::sobel(&intensity, frame.w, frame.h);
+
+        let mut magnitude = vec![0.0; frame.w * frame.h];
+        let mut angle = vec![0.0; frame.w * frame.h];
+
+        // for each pixel...
+        for i in 0..gx.len() {
+            // get the strength / intensity of the edge
+            magnitude[i] = (gx[i] * gx[i] + gy[i] * gy[i]).sqrt();
+            // get the direction of the edge
+            angle[i] = gy[i].atan2(gx[i]);
+        }
+
+        // thin edges & remove edges that are most likely just noise
+        let magnitude = Self::non_maximum_suppression(
+            &magnitude,
+            &angle,
+            frame.w,
+            frame.h,
+            threshold
+        );
+
+        Ok((magnitude, angle))
+    }
+
+    pub fn get_edge_info(&self) -> Result<EdgeInfo, Box<dyn Error>> {
+        let edge_info = self.edge_info.lock().unwrap();
+
+        Ok(EdgeInfo {
+            magnitude: edge_info.magnitude.clone(),
+            angle: edge_info.angle.clone(),
+            w: edge_info.w,
+            h: edge_info.h,
+        })
+    }
+
+    pub fn stop(&self) {
+        let mut running = self.running.lock().unwrap();
+        *running = false;
+    }
+
+    /// Extracts intensity values from an RGB image to be used 
+    /// for edge detection
+    fn create_intensity_map(frame: &ImageFrame) -> Vec<f32> {
+        let mut intensity = vec![0.0; frame.w * frame.h];
+
+        for y in 0..frame.h {
+            for x in 0..frame.w {
+                if let Some((r, g, b)) = frame.get_pixel(x, y) {
+                    let gray = ImageFrame::calculate_intensity((r, g, b));
+                    intensity[y * frame.w + x] = gray;
+                }
+            }
+        }
+
+        intensity
+    }
+
+    /// Applies the Sobel operator to a matrix containing the intensities of
+    /// a processed `ImageFrame`. This is utilized for edge detection in the
+    /// image.
+    ///
+    /// The Sobel kernels are defined as follows:
+    /// - `Gx = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]`
+    /// - `Gy = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]`
+    fn sobel(intensity: &[f32], w: usize, h: usize) -> (Vec<f32>, Vec<f32>) {
+        let mut gx = vec![0.0; w * h];
+        let mut gy = vec![0.0; w * h];
+
+        for y in 1..(h - 1) {
+            for x in 1..(w - 1) {
+                let i = y * w + x;
+
+                // skipping over entries w/ 0 due to initialization
+                gx[i] =
+                    -1.0 * intensity[(y - 1) * w + (x - 1)] + // Gx(0,0)
+                        1.0 * intensity[(y - 1) * w + (x + 1)] +  // Gx(0,2)
+                        -2.0 * intensity[y * w + (x - 1)] +       // Gx(1,0)
+                        2.0 * intensity[y * w + (x + 1)] +        // Gx(1,2)
+                        -1.0 * intensity[(y + 1) * w + (x - 1)] + // Gx(2,0)
+                        1.0 * intensity[(y + 1) * w + (x + 1)];   // Gx(2,2)
+
+                gy[i] =
+                    -1.0 * intensity[(y - 1) * w + (x - 1)] + // Gy(0,0)
+                        -2.0 * intensity[(y - 1) * w + x] +       // Gy(0,1)
+                        -1.0 * intensity[(y - 1) * w + (x + 1)] + // Gy(0,2)
+                        1.0 * intensity[(y + 1) * w + (x - 1)] +  // Gy(2,0)
+                        2.0 * intensity[(y + 1) * w + x] +        // Gy(2,1)
+                        1.0 * intensity[(y + 1) * w + (x + 1)];   // Gy(2,2)
+            }
+        }
+
+        (gx, gy)
+    }
+
+    /// Performs non-maximum suppression on a gradient magnitude to thin edges.
+    ///
+    /// By examining each pixel and its neighbors along the gradient direction, 
+    /// the function determines a local maximum. Only pixels that meet / exceed 
+    /// the local maximum and exceed the threshold are preserved.
+    ///
+    /// This will reduce the thickness of edges to a single-pixel width and
+    /// remove edge points that are more than likely noise.
+    fn non_maximum_suppression(magnitude: &[f32], angle: &[f32], w: usize, h: usize, threshold: f32) -> Vec<f32> {
+        let mut result = vec![0.0; w * h];
+
+        for y in 1..(h - 1) {
+            for x in 1..(w - 1) {
+                let i = y * w + x;
+
+                // below magnitude? weak edge, skip
+                if magnitude[i] < threshold {
+                    continue;
+                }
+
+                // normalize to 0-180 degrees
+                let angle_deg = (angle[i].to_degrees() + 180.0) % 180.0;
+
+                let (nx1, ny1, nx2, ny2) = if (angle_deg >= 0.0 && angle_deg < 22.5) || (angle_deg >= 157.5 && angle_deg < 180.0) {
+                    // horizontal edge
+                    (x+1, y, x-1, y)
+                } else if angle_deg >= 22.5 && angle_deg < 67.5 {
+                    // forward edge (/)
+                    (x+1, y-1, x-1, y+1)
+                } else if angle_deg >= 67.5 && angle_deg < 112.5 {
+                    // vertical edge
+                    (x, y-1, x, y+1)
+                } else {
+                    // back edge (\)
+                    (x-1, y-1, x+1, y+1)
+                };
+
+                // compare with neighboring values
+                let n1 = if nx1 < w && ny1 < h {
+                    magnitude[ny1 * w + nx1]
+                } else {
+                    0.0
+                };
+
+                let n2 = if nx2 < w && ny2 < h {
+                    magnitude[ny2 * w + nx2]
+                } else {
+                    0.0
+                };
+
+                // Keep only local maxima
+                if magnitude[i] >= n1 && magnitude[i] >= n2 {
+                    result[i] = magnitude[i];
+                }
+            }
+        }
+
+        result
+    }
+}
