592c0de0fcf9c67ba43a5c4f2fab2de2bcf68999
rafeloya <rafe_loya1@baylor.edu>
2025-04-25 19:04:51 -0500

    Very basic server implementation

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,5 @@
+extern crate alloc;
+
 mod ffmpeg;
 mod camera;
 mod ascii_renderer;
@@ -5,7 +7,9 @@ mod image_frame;
 mod ascii_converter;
 mod edge_detector;
 mod video_config;
+mod client;
 
+use std::net::SocketAddr;
 use crate::ascii_converter::AsciiConverter;
 use common::ascii_frame::AsciiFrame;
 use crate::ascii_renderer::AsciiRenderer;
@@ -15,56 +19,71 @@ use crate::image_frame::ImageFrame;
 use crate::video_config::VideoConfig;
 use std::time::Duration;
 use std::thread;
+use crate::client::Client;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = VideoConfig::new(
-        640,
-        480,
-        120,
-        40,
-        127.50,
-        1.5,
-        0.0
-    );
-
-    let mut camera = Camera::new(config.camera_width, config.camera_height)?;
-
-    let mut image_frame = ImageFrame::new(config.camera_width, config.camera_height, 3)?;
-    let mut ascii_frame = AsciiFrame::new(config.ascii_width, config.ascii_height, ' ')?;
-
-    let converter = AsciiConverter::new(
-        AsciiConverter::DEFAULT_ASCII_INTENSITY.chars().collect(),
-        AsciiConverter::DEFAULT_ASCII_HORIZONTAL.chars().collect(),
-        AsciiConverter::DEFAULT_ASCII_VERTICAL.chars().collect(),
-        AsciiConverter::DEFAULT_ASCII_FORWARD.chars().collect(),
-        AsciiConverter::DEFAULT_ASCII_BACK.chars().collect(),
-        config.camera_width,
-        config.camera_height,
-        config.edge_threshold,
-        config.contrast,
-        config.brightness
-    )?;
-
-    let mut renderer = AsciiRenderer::new()?;
-
-    loop {
-        if let Err(e) = camera.capture_frame(&mut image_frame) {
-            eprintln!("failed while capturing frame: {}", e);
-            break;
-        }
-
-        if let Err(e) = converter.convert(&image_frame, &mut ascii_frame) {
-            eprintln!("failed while converting frame: {}", e);
-            break;
-        }
-
-        if let Err(e) = renderer.render(&ascii_frame) {
-            eprintln!("failed while rendering frame: {}", e);
-            break;
-        }
-
-        thread::sleep(Duration::from_millis(10));
-    }
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let mut config = VideoConfig::new(
+    //     640,
+    //     480,
+    //     120,
+    //     40,
+    //     127.50,
+    //     1.5,
+    //     0.0
+    // );
+    // 
+    // let mut camera = Camera::new(config.camera_width, config.camera_height)?;
+    // 
+    // let mut image_frame = ImageFrame::new(config.camera_width, config.camera_height, 3)?;
+    // let mut ascii_frame = AsciiFrame::new(config.ascii_width, config.ascii_height, ' ')?;
+    // 
+    // let converter = AsciiConverter::new(
+    //     AsciiConverter::DEFAULT_ASCII_INTENSITY.chars().collect(),
+    //     AsciiConverter::DEFAULT_ASCII_HORIZONTAL.chars().collect(),
+    //     AsciiConverter::DEFAULT_ASCII_VERTICAL.chars().collect(),
+    //     AsciiConverter::DEFAULT_ASCII_FORWARD.chars().collect(),
+    //     AsciiConverter::DEFAULT_ASCII_BACK.chars().collect(),
+    //     config.camera_width,
+    //     config.camera_height,
+    //     config.edge_threshold,
+    //     config.contrast,
+    //     config.brightness
+    // )?;
+    // 
+    // let mut renderer = AsciiRenderer::new()?;
+    // 
+    // loop {
+    //     if let Err(e) = camera.capture_frame(&mut image_frame) {
+    //         eprintln!("failed while capturing frame: {}", e);
+    //         break;
+    //     }
+    // 
+    //     if let Err(e) = converter.convert(&image_frame, &mut ascii_frame) {
+    //         eprintln!("failed while converting frame: {}", e);
+    //         break;
+    //     }
+    // 
+    //     if let Err(e) = renderer.render(&ascii_frame) {
+    //         eprintln!("failed while rendering frame: {}", e);
+    //         break;
+    //     }
+    // 
+    //     thread::sleep(Duration::from_millis(10));
+    // }
+    
+    tracing_subscriber::fmt::init();
+    
+    let mut client = Client::new()?;
+    
+    let server_addr = "[::1]:4433".parse::<SocketAddr>()?;
+    client.connect(server_addr, "localhost").await?;
+    
+    let response = client.send_message(b"hello from client!").await?;
+    println!("response: {:?}", std::str::from_utf8(&response)?);
+    
+    client.close();
+    client.wait_idle().await;
 
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,7 +1,22 @@
+use std::error::Error;
+use std::net::SocketAddr;
+use crate::server::Server;
+
 mod server;
 
 /// TODO: Look into multiple streams to avoid head-of-line blocking: does QUIC / quinn automatically do this
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
     
+    let addr = "[::1]:4433".parse::<SocketAddr>()?;
+    let server = Server::new(addr)?;
+    
+    println!("Server listening on {}", server.local_addr()?);
+    
+    // blocking call
+    server.run().await?;
+    
+    Ok(())
 }
