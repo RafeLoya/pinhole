66256cea797d313c4db5080f96f9b896714de8a9
wesleygoyette <wesley.goyette@gmail.com>
2025-05-02 20:36:32 -0500

    Removed ASCII camera code to focus on server-client functionality

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,144 +1,3 @@
-// use std::error::Error;
-// use std::net::SocketAddr;
-// use std::sync::Arc;
-// use quinn::{ClientConfig, Connection, Endpoint};
-// use rustls::client::danger;
-// use rustls::crypto::{verify_tls12_signature, verify_tls13_signature, CryptoProvider};
-// use rustls::{DigitallySignedStruct, SignatureScheme};
-// use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
-// use tracing::info;
-// use quinn::SendStream;
-// use quinn::RecvStream;
-// #[derive(Debug)]
-// struct SkipServerVerification(Arc<CryptoProvider>);
-//
-// impl SkipServerVerification {
-//     fn new() -> Arc<Self> {
-//         Arc::new(Self(Arc::new(rustls::crypto::ring::default_provider())))
-//     }
-// }
-//
-// impl danger::ServerCertVerifier for SkipServerVerification {
-//     fn verify_server_cert(
-//         &self,
-//         _end_entity: &CertificateDer<'_>,
-//         _intermediates: &[CertificateDer<'_>],
-//         _server_name: &ServerName<'_>,
-//         _ocsp: &[u8],
-//         _now: UnixTime,
-//     ) -> Result<danger::ServerCertVerified, rustls::Error> {
-//         Ok(danger::ServerCertVerified::assertion())
-//     }
-//     fn verify_tls12_signature(
-//         &self,
-//         message: &[u8],
-//         cert: &CertificateDer<'_>,
-//         dss: &DigitallySignedStruct,
-//     ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
-//         verify_tls12_signature(
-//             message,
-//             cert,
-//             dss,
-//             &self.0.signature_verification_algorithms,
-//         )
-//     }
-//
-//     fn verify_tls13_signature(
-//         &self,
-//         message: &[u8],
-//         cert: &CertificateDer<'_>,
-//         dss: &DigitallySignedStruct,
-//     ) -> Result<danger::HandshakeSignatureValid, rustls::Error> {
-//         verify_tls13_signature(
-//             message,
-//             cert,
-//             dss,
-//             &self.0.signature_verification_algorithms,
-//         )
-//     }
-//
-//     fn supported_verify_schemes(&self) -> Vec<SignatureScheme> {
-//         self.0.signature_verification_algorithms.supported_schemes()
-//     }
-// }
-//
-// pub struct Client {
-//     endpoint: Endpoint,
-//     connection: Option<Connection>,
-// }
-//
-// impl Client {
-//     pub fn new() -> Result<Self, Box<dyn Error>> {
-//         let client_config = Self::configure_client()?;
-//
-//         let bind_addr = "[::]:0".parse::<SocketAddr>()?;
-//         let mut endpoint = Endpoint::client(bind_addr)?;
-//         endpoint.set_default_client_config(client_config);
-//
-//         Ok(Self {
-//             endpoint,
-//             connection: None,
-//         })
-//     }
-//
-//     pub async fn connect(&mut self, server_addr: SocketAddr, server_name: &str) -> Result<(), Box<dyn Error>> {
-//         let connecting = self.endpoint
-//             .connect(server_addr, server_name)?;
-//         let connection = connecting.await?;
-//
-//         info!("connected to server: {}", connection.remote_address());
-//
-//         self.connection = Some(connection);
-//
-//         Ok(())
-//     }
-//
-//     pub async fn send_message(&self, message: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
-//         if let Some(conn) = self.connection.as_ref() {
-//             let (mut send, mut recv) = conn.open_bi().await?;
-//
-//             send.write_all(&message).await?;
-//             send.finish()?;
-//
-//             let response = recv.read_to_end(1024).await?;
-//             info!("received message: {:?}", response);
-//
-//             Ok(response)
-//         } else {
-//             Err("not connected to any server".into())
-//         }
-//     }
-//
-//     pub fn close(&self) {
-//         if let Some(conn) = &self.connection {
-//             conn.close(0u32.into(), b"done");
-//         }
-//     }
-//
-//     pub async fn wait_idle(&self) {
-//         self.endpoint.wait_idle().await;
-//     }
-//
-//     fn configure_client() -> Result<ClientConfig, Box<dyn Error>> {
-//         rustls::crypto::ring::default_provider()
-//             .install_default()
-//             .expect("failed to install rustls crypto provider");
-//
-//         let mut crypto = rustls::ClientConfig::builder()
-//             .dangerous() // wot is this?
-//             .with_custom_certificate_verifier(SkipServerVerification::new())
-//             .with_no_client_auth();
-//
-//         crypto.alpn_protocols = vec![b"h3".to_vec()];
-//
-//         let quinn_crypto = quinn::crypto::rustls::QuicClientConfig::try_from(crypto)?;
-//
-//         let client_config =  ClientConfig::new(Arc::new(quinn_crypto));
-//
-//         Ok(client_config)
-//     }
-// }
-
 use std::error::Error;
 use std::io::{Read, Write};
 use std::net::{TcpStream, UdpSocket};
@@ -146,11 +5,7 @@ use std::sync::{Arc, Mutex};
 use tokio::sync::mpsc;
 use tokio::task;
 use common::ascii_frame::AsciiFrame;
-use crate::ascii_converter::AsciiConverter;
 use crate::ascii_renderer::AsciiRenderer;
-use crate::camera::Camera;
-use crate::image_frame::ImageFrame;
-use crate::video_config::VideoConfig;
 
 pub struct Client {
     /// TCP address for 'control' messages (i.e. updates to session state)
@@ -245,56 +100,19 @@ impl Client {
         
         // WEBCAM LOGIC
         //=====================================================================
-        let config = VideoConfig::new(
-            640,
-            480,
-            120,
-            40,
-            127.50,
-            1.5, 
-            0.0
-        );
-        
-        let mut camera = Camera::new(config.camera_width, config.camera_height)?;
-        
-        let mut image_frame = ImageFrame::new(config.camera_width, config.camera_height, 3)?;
-        let mut ascii_frame = AsciiFrame::new(config.ascii_width, config.ascii_height, ' ')?;
-        
-        let converter = AsciiConverter::new(
-            AsciiConverter::DEFAULT_ASCII_INTENSITY.chars().collect(),
-            AsciiConverter::DEFAULT_ASCII_HORIZONTAL.chars().collect(),
-            AsciiConverter::DEFAULT_ASCII_VERTICAL.chars().collect(),
-            AsciiConverter::DEFAULT_ASCII_FORWARD.chars().collect(),
-            AsciiConverter::DEFAULT_ASCII_BACK.chars().collect(),
-            config.camera_width,
-            config.camera_height,
-            config.edge_threshold,
-            config.contrast,
-            config.brightness
-        )?;
         
         while *self.is_connected.lock().unwrap() {
             if *self.has_peer.lock().unwrap() {
-                if let Err(e) = camera.capture_frame(&mut image_frame) {
-                    eprintln!("failed to capture frame: {}", e);
-                    continue;
-                }
                 
-                if let Err(e) = converter.convert(&image_frame, &mut ascii_frame) {
-                    eprintln!("failed to convert frame: {}", e);
-                    continue;
-                }
-                
-                let mut frame_to_send = AsciiFrame::new(config.ascii_width, config.ascii_height, ' ')?;
-                frame_to_send.set_chars_from_bytes(&ascii_frame.bytes());
-                if let Err(e) = frame_tx.try_send(frame_to_send) {
+                let frame = AsciiFrame::from_bytes(3, 1, &[0u8, 0, 0])?;
+
+                if let Err(e) = frame_tx.try_send(frame) {
                     eprintln!("failed to send ascii frame: {}", e);
                 }
             }
             
             // originally 10ms
             tokio::time::sleep(tokio::time::Duration::from_millis(33)).await;
-            //std::thread::sleep(std::time::Duration::from_millis(33));
         }
         //=====================================================================
         
--- client/src/main.rs
+++ client/src/main.rs
@@ -10,7 +10,7 @@ mod video_config;
 mod client;
 
 use clap::Parser;
-use rand::{rng, Rng};
+use rand::{Rng};
 use crate::client::Client;
 
 #[derive(Parser, Debug)]
@@ -33,74 +33,8 @@ struct Args {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let mut config = VideoConfig::new(
-    //     640,
-    //     480,
-    //     120,
-    //     40,
-    //     127.50,
-    //     1.5,
-    //     0.0
-    // );
-    //
-    // let mut camera = Camera::new(config.camera_width, config.camera_height)?;
-    //
-    // let mut image_frame = ImageFrame::new(config.camera_width, config.camera_height, 3)?;
-    // let mut ascii_frame = AsciiFrame::new(config.ascii_width, config.ascii_height, ' ')?;
-    //
-    // let converter = AsciiConverter::new(
-    //     AsciiConverter::DEFAULT_ASCII_INTENSITY.chars().collect(),
-    //     AsciiConverter::DEFAULT_ASCII_HORIZONTAL.chars().collect(),
-    //     AsciiConverter::DEFAULT_ASCII_VERTICAL.chars().collect(),
-    //     AsciiConverter::DEFAULT_ASCII_FORWARD.chars().collect(),
-    //     AsciiConverter::DEFAULT_ASCII_BACK.chars().collect(),
-    //     config.camera_width,
-    //     config.camera_height,
-    //     config.edge_threshold,
-    //     config.contrast,
-    //     config.brightness
-    // )?;
-    //
-    // let mut renderer = AsciiRenderer::new()?;
-    //
-    // loop {
-    //     if let Err(e) = camera.capture_frame(&mut image_frame) {
-    //         eprintln!("failed while capturing frame: {}", e);
-    //         break;
-    //     }
-    //
-    //     if let Err(e) = converter.convert(&image_frame, &mut ascii_frame) {
-    //         eprintln!("failed while converting frame: {}", e);
-    //         break;
-    //     }
-    //
-    //     if let Err(e) = renderer.render(&ascii_frame) {
-    //         eprintln!("failed while rendering frame: {}", e);
-    //         break;
-    //     }
-    //
-    //     thread::sleep(Duration::from_millis(10));
-    // }
-
-    // tracing_subscriber::fmt::init();
-    // 
-    // let mut client = Client::new()?;
-    // 
-    // let server_addr = "[::1]:4433".parse::<SocketAddr>()?;
-    // client.connect(server_addr, "localhost").await?;
-    // 
-    // let response = client.send_message(b"hello from client!").await?;
-    // println!("response: {:?}", std::str::from_utf8(&response)?);
-    // 
-    // client.close();
-    // client.wait_idle().await;
-    // 
-    // Ok(())
-    
-    // TODO: mock client program?
     
     let args = Args::parse();
-    tracing_subscriber::fmt::init();
     
     let session_id = if args.session_id.is_empty() {
         let rand_id: u32 = rand::rng().random();
